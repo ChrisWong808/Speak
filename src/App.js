bcc875e8547762d6a4fe2716df24ee0f69e8806c
Chris Wong <wongcpie@gmail.com>
2023-01-11 22:19:04 -1000

    MVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, {useEffect, useState, useRef} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import pikaPic from './pikachu.jpeg';
+import pikaGif from './red.gif';
+import msGif from './ms2.gif';
+import dogGif from './dog.gif';
+import babyGif from './baby.gif';
+import gokuGif from './goku.gif';
+import lazydogGif from './lazydog.gif';
+import pusheenGif from './pusheen.gif';
+import fartGif from './fart.gif';
+import flydogGif from './flydog.gif';
+import rolldogGif from './rolldog.gif';
+import foxGif from './fox.gif';
+import charlieDance from './charlieDance.gif';
+import ad from './ad.gif';
+import Sound from 'react-sound';
+import song from './Hunt.mp3';
+import speak from './speak.png';
 // https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
 
 // 0. Import depdendencies
@@ -8,19 +25,24 @@ import * as tf from "@tensorflow/tfjs"
 import * as speech from "@tensorflow-models/speech-commands"
 
 // 4. Draw Ball
-import {drawBall} from "./utilities";
+// import {drawBall} from "./utilities";
 
 const App = () => {
+const [isPlaying, setIsPlaying] = useState(false);
+const [gif, setGif] = useState(pikaGif)
 // 1. Create model and action states
 const [model, setModel] = useState(null)
 const [action, setAction] = useState(null)
 const [labels, setLabels] = useState(null)
+// const [one, setOne] = useState({p:msGif, c:0})
 
 // 6. Create Canvas Ref and x,y,r
-const canvasRef = useRef(null); 
-const [x, setX] = useState(300)
-const [y, setY] = useState(300)
-const [r, setR] = useState(10)
+// const canvasRef = useRef(null);
+const [x, setX] = useState(600)
+const [y, setY] = useState(350)
+// const [r, setR] = useState(10)
+const [donate, setDonate] = useState(100);
+const [change, setChange] = useState(0)
 
 // 2. Create Recognizer
 const loadModel = async () =>{
@@ -32,36 +54,56 @@ const loadModel = async () =>{
   setLabels(recognizer.wordLabels())
 }
 
-useEffect(()=>{loadModel()}, []); 
+useEffect(()=>{loadModel()}, []);
 
 // 7. Update ball state
 const numberMap = {
-  "zero":0,
-  "one":1,
-  "two":2,
-  "three":3,
-  "four":4,
-  "five":5,
-  "six":6,
-  "seven":7,
-  "eight":8,
-  "nine":9
+  "zero":pikaGif,
+  // "one":one.p,
+  "one":msGif,
+  "two":dogGif,
+  "three":gokuGif,
+  "four":lazydogGif,
+  "five":pusheenGif,
+  "six":flydogGif,
+  "seven":rolldogGif,
+  "eight":foxGif,
+  "nine":fartGif,
 }
 
 useEffect(()=>{
   // Update position x,y
-  const update = action === 'up' ? setY(y-10) : action==="down" ? setY(y+10) : action==="left" ? setX(x-10) : action==="right"? setX(x+10) : ""
+  const update = action === 'up' ? setY(y-100) : action==="down" ? setY(y+100) : action==="left" ? setX(x-100) : action==="right"? setX(x+100) : ''
+  //action==="go" ? setIsPlaying(true) : action==="stop" ? setIsPlaying(false) : ''
   // Update size r
   if(Object.keys(numberMap).includes(action)){
-    setR(10*numberMap[action])
+    // setR(10*numberMap[action])
+    // setGif({p: numberMap[action]})
+    setGif(numberMap[action])
+  }
+  if (action==="go") {
+    setIsPlaying(true)
+  }
+  if (action==="stop") {
+    setIsPlaying(false)
   }
 
-  canvasRef.current.width = 600;
-  canvasRef.current.height = 600;
-  const ctx = canvasRef.current.getContext('2d')
-  console.log(x,y,r)
-  drawBall(ctx,x,y,r)
-  setAction('base')
+  // canvasRef.current.width = 600;
+  // canvasRef.current.height = 600;
+  // const ctx = canvasRef.current.getContext('2d')
+  // let canvas = document.getElementById('canvas');
+  // const ctx = canvas.getContext('2d')
+  // console.log(x,y,r)
+  // drawBall(ctx,x,y,r)
+
+  // const image = new Image();
+  // image.src = './pikachu.jpeg';
+  // image.onload = () => {
+  //   ctx.drawImage(image, x, y);
+  // };
+
+  // setAction('base')
+
 }, [action])
 
 // 3. Listen for Actions
@@ -74,34 +116,101 @@ const recognizeCommands = async () =>{
   model.listen(result=>{
     // console.log(labels[argMax(Object.values(result.scores))])
     setAction(labels[argMax(Object.values(result.scores))])
-    
+
   }, {includeSpectrogram:true, probabilityThreshold:0.9})
   // setTimeout(()=>model.stopListening(), 10e3)
 }
 
+  const handleChange = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* 5. Setup Canvas */}
-        <canvas 
-        ref={canvasRef}
+      {/* <h1>Speak</h1>
+      <h2>Find your Voice</h2> */}
+      <img src={speak} alt=''/>
+      {/* <img src={ad} alt='' style = {{
+        position: 'absolute',
+        top: '50px',
+        right:'25px',
+        height: '200px',
+        width: '150px',
+      }}/> */}
+      <button onClick={recognizeCommands}>Command</button>
+          {action ? <div>{action}</div>:<div>No Action Detected</div> }
+      <img src={gif} alt='' width='250' height='200'
         style={{
+          position: "absolute",
+          left: `${x}px`,
+          top: `${y}px`,
+        }}
+      />
+      <div style={{
+        position: 'absolute',
+        left: '125px',
+      }
+      }>
+        <div className='CL'>Command List</div>
+        <div>#0-9 --> Image</div>
+        <div>Direction --> Move</div>
+        <div>Go/Stop --> Music</div>
+      </div>
+
+        {/* 5. Setup Canvas */}
+        <canvas id='canvas'
+        // ref={canvasRef}
+        style={{
+          // backgroundColor: 'red',
           marginLeft: "auto",
           marginRight: "auto",
           left: 0,
           right: 0,
           textAlign: "center",
           zindex: 9,
-          width: 640,
-          height: 640,
+          width: 465,
+          height: 465,
         }}
         />
+        {/* <div>
+          <div>one : {one.c}</div>
+          <div></div>
+        </div> */}
+        <div className='miss' >Our mission is to help those that have lost their voice. Whether you are technologically challenged, visually impaired, or simply looking to simplify things, we are here to increase your quality of life through voice recognition and control. </div>
+      <div className='suffer'>Don't Suffer in Silence</div>
+      < img src={charlieDance} alt='' width='90' height='150'
+      />
+      <button onClick={()=>{setIsPlaying(!isPlaying)}}>{!isPlaying ? 'Play Music' : 'Stop Music'}</button>
+      <Sound
+        url={song}
+        playStatus={
+          isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
+        }
+      />
+      <div>
+         <form onSubmit={(e) => {
+        e.preventDefault();
+        setDonate(Number(donate) + Number(change));
+        window.prompt('Thank you for your generous donation!', 'I will donate more later on');
+        }}>
+        <label>How much money would you like to donate?</label>
+        <input value={change} onChange={(e)=>{setChange(e.target.value)}}>
+      </input>
+      <button type='submit'>Donate</button>
+      </form>
+      <div>Raised so far: ${donate}</div>
+      </div>
+
+      {/* <img src={pikaPic} alt=''
+      /> */}
+      {/* <img src={msGif} alt=''/> */}
+      {/* <div>Pikachu: Send Help D:</div> */}
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
-          <button onClick={recognizeCommands}>Command</button>
-          {action ? <div>{action}</div>:<div>No Action Detected</div> }
+
       </header>
     </div>
   );
--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,35 @@
-export const drawBall = (ctx, x, y, r)=>{
+// import pic from './pikachu.jpeg'
+// const pic = document.getElementById('pikachu.jpeg')
+{/* <img src = './pikachu.jpeg' ></img> */}
 
+export const drawBall = (ctx, x, y, r) => {
     ctx.beginPath();
+    console.log('it is running')
     ctx.arc(x, y, r, 0, 3*Math.PI)
     ctx.fillStyle = "aqua"
     ctx.fill()
-} 
+
+    var img = new Image();
+    img.src = './pikachu.jpeg';
+    img.onload = function() {
+        console.log('on load')
+        console.log(img.width,img.height,img.complete);
+        // ctx.save();
+        // ctx.globalCompositeOperation = 'source-in';
+        ctx.drawImage(img, x, y)
+        // ctx.restore();
+      }
+    // img.src = './pikachu.jpeg';
+    // img.decode().then(()=>{
+    //   console.log('on load')
+    //   ctx.drawImage(img, x, y)
+    // }).catch(()=>{
+    //     console.log('Error loading image')
+    // });
+    // if(img.complete){
+    //     console.log('on load')
+    //     ctx.drawImage(img, x, y);
+    // }
+
+    //ctx.drawImage(img, x, y)
+}
